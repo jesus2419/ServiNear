edae018d23102e690a3ca651ed1973a98532186f
jesus2419 <102651007+jesus2419@users.noreply.github.com>
2024-05-01 17:02:29 -0600

    1.0
    agregamos diseño a las pantallas en un 30%

--- app/app/src/main/java/com/example/servinear/MainActivity.kt
+++ app/app/src/main/java/com/example/servinear/MainActivity.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(2000)
+        setTheme(R.style.splashtheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btn: Button = findViewById(R.id.register_btn)
