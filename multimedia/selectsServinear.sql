5446d4059ed65d4ec1c7369337ddd854b2e485a5
jesus2419 <102651007+jesus2419@users.noreply.github.com>
2024-06-07 04:03:41 -0600

    Update selectsServinear.sql

--- multimedia/selectsServinear.sql
+++ multimedia/selectsServinear.sql
@@ -4,6 +4,8 @@ SELECT Nombre, descripcion, contacto,precio_hora, Foto FROM servicio WHERE Estad
 SELECT ID, id_usuario, Nombre, descripcion, contacto, precio_hora, Foto, Fecha_de_creacion, Estado FROM servicio WHERE Estado = 1 ;
 
 select * from usuarios;
+select * from chat;
+
 
 UPDATE usuarios
 SET Nombre  = ?, Apellidos  = ?, Correo  = ?, pass  = ?, Foto  = ?
@@ -68,4 +70,65 @@ SELECT
             JOIN 
                 Usuarios ON servicio.id_usuario = Usuarios.ID
             WHERE 
-                Usuarios.usuario = 'jesus6'
+                Usuarios.usuario = 'jesus6';
+                
+                
+                
+                
+SELECT 
+    c.ID AS ChatID,
+    c.contenido,
+    c.fecha_de_creacion,
+    c.estado,
+    u1.usuario AS Remitente,
+    u2.usuario AS Destinatario
+FROM 
+    chat c
+JOIN 
+    Usuarios u1 ON c.id_remitente = u1.ID
+JOIN 
+    Usuarios u2 ON c.id_destinatario = u2.ID where c.id_destinatario = 3 or c.id_remitente = 3 or  c.id_destinatario = 7 or c.id_remitente = 7 ;
+    select * from chat;
+
+
+
+SELECT 
+    c.ID AS ChatID,
+    c.contenido,
+    c.fecha_de_creacion,
+    c.estado,
+    u1.usuario AS Remitente,
+    u2.usuario AS Destinatario
+FROM 
+    chat c
+JOIN 
+    Usuarios u1 ON c.id_remitente = u1.ID
+JOIN 
+    Usuarios u2 ON c.id_destinatario = u2.ID
+WHERE 
+    c.id_destinatario IN (3, 7) OR c.id_remitente IN (3, 7)
+ORDER BY 
+    c.fecha_de_creacion ASC;
+
+
+SELECT 
+    c.ID AS ChatID,
+    c.contenido,
+    c.fecha_de_creacion,
+    c.estado,
+    u1.usuario AS Remitente,
+    u2.usuario AS Destinatario
+FROM 
+    chat c
+JOIN 
+    Usuarios u1 ON c.id_remitente = u1.ID
+JOIN 
+    Usuarios u2 ON c.id_destinatario = u2.ID
+WHERE 
+    (u1.usuario = 'jesus2' AND u2.ID = 3) OR
+    (u1.usuario = 'jesus2' AND u2.usuario = 'jesus2') OR
+    (u1.ID = 3 AND u2.usuario = 'jesus2') OR
+    (u1.ID = 3 AND u2.ID = 3)
+ORDER BY 
+    c.fecha_de_creacion ASC;
+
